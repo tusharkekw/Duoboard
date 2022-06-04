9d8d118e2919eeff24bdf3b27d172d5cd36c4b25
tusharkekw <tushar.khatri.it19@nsut.ac.in>
2022-06-05 03:59:58 +0530

    added socket.io support

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express(); //Initialized and server ready
 app.use(express.static("public"));
 
 let port = process.env.PORT || 5000;
+
 let server = app.listen(port, () => {
     console.log("Listening to port" + port);
 })
